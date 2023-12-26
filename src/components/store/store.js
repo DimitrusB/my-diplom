a2fcc4fb53c85e820245af5e804c6ffccfa5ed41
dmitriy <admin@silverboats.ru>
2023-12-26 10:12:13 +0300

    del store

--- src/components/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import adsReducer from "./reducers/reducers";
-
-export const store = configureStore({
-    reducer: {
-        adsReducer: adsReducer,
-    },
-})
